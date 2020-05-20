5545ee8d3c80cda04a91cfa37a66fa3e57edce57
mihir-codes <mihirsinh.parmar.it@gmail.com>
2020-05-20 11:33:31 +0530

    Updates

---
+++ precache-manifest.c15c61b09e274c967082af0404c1382d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c2bb14affcca0d2c7682244c3260d94",
+    "url": "/bankchain-client/index.html"
+  },
+  {
+    "revision": "27608e8750cb8398a4b7",
+    "url": "/bankchain-client/static/css/2.d811125f.chunk.css"
+  },
+  {
+    "revision": "8d2b598cc6fd8c19c3c5",
+    "url": "/bankchain-client/static/css/main.bc5d9192.chunk.css"
+  },
+  {
+    "revision": "27608e8750cb8398a4b7",
+    "url": "/bankchain-client/static/js/2.a3e28ce0.chunk.js"
+  },
+  {
+    "revision": "e37455db86ace3107380a24ad79003cb",
+    "url": "/bankchain-client/static/js/2.a3e28ce0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d2b598cc6fd8c19c3c5",
+    "url": "/bankchain-client/static/js/main.9c77ed5a.chunk.js"
+  },
+  {
+    "revision": "67edb9d691d8d3fc3e2c",
+    "url": "/bankchain-client/static/js/runtime-main.ee2faf8a.js"
+  }
+]);
